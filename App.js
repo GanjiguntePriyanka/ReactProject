22f70a266a3cdf523d1fd4f5528ee48476af2c24
GanjiguntePriyanka <priyanka.gsmurthy@gmail.com>
2024-02-04 11:16:49 +0000

    chapter4- swiggy with static cards

--- App.js
+++ App.js
@@ -21,10 +21,42 @@ const Header = () => {
 
     )
 }
+//const resCardStyle={backgroundColor:"yellow"}
+
+const RestuarantCard = ()=>{
+    return(
+        // <div className="res-card" style={resCardStyle}>
+        // <div className="res-card" style={{backgroundColor:"yellow"}}>
+        <div className="res-card">
+            <img className = "res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/121f77969f3c28f1d7cbcb71e7aa23e0"></img>
+            <h3>Biriyani Foods</h3>
+            <h4>3.4 stars</h4>
+            <h4>Indian, Rice</h4>
+            <h4>35 min</h4>
+            <div></div>
+        </div>
+    )
+}
+
+
+const Body =()=>{
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestuarantCard/>
+                <RestuarantCard/>
+                <RestuarantCard/>
+                <RestuarantCard/>
+            </div>
+        </div>
+    )
+}
 const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
