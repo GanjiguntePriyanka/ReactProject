ecfd833f53df8a1d25f429c6a959ebf9cdc1ad7a
GanjiguntePriyanka <priyanka.gsmurthy@gmail.com>
2024-02-04 11:37:24 +0000

    chapter 4 - passing PROPS to components

--- App.js
+++ App.js
@@ -23,16 +23,14 @@ const Header = () => {
 }
 //const resCardStyle={backgroundColor:"yellow"}
 
-const RestuarantCard = ()=>{
+const RestuarantCard = (props)=>{
     return(
-        // <div className="res-card" style={resCardStyle}>
-        // <div className="res-card" style={{backgroundColor:"yellow"}}>
         <div className="res-card">
             <img className = "res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/121f77969f3c28f1d7cbcb71e7aa23e0"></img>
-            <h3>Biriyani Foods</h3>
-            <h4>3.4 stars</h4>
-            <h4>Indian, Rice</h4>
-            <h4>35 min</h4>
+            <h3>{props.restroName}</h3>
+            <h4>{props.starRating}</h4>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.deliveryTime}</h4>
             <div></div>
         </div>
     )
@@ -44,10 +42,8 @@ const Body =()=>{
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestuarantCard/>
-                <RestuarantCard/>
-                <RestuarantCard/>
-                <RestuarantCard/>
+                <RestuarantCard restroName="Biriyani Foods" cuisine = "Indian, Rice" starRating="3.4 stars" deliveryTime="44 min"/>
+                <RestuarantCard restroName="KFC" cuisine="British, street food" starRating="4 stars" deliveryTime="22 min"/>
             </div>
         </div>
     )
