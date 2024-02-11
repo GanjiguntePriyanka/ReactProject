466e25b5b60ca5a799eb73bcdb1340dedc3f3999
GanjiguntePriyanka <priyanka.gsmurthy@gmail.com>
2024-02-11 17:51:46 +0000

    chapter5_usingStateVariables for buttonClick

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,28 @@
 import cardsFromSwiggy from "../utils/mockData";
 import RestuarantCard from "./RestuarantCard";
+import { useState } from "react";
 
 const Body = () => {
+    //use State variable
+    const [listOfRestaurants, setListOfRestraunt] = useState(cardsFromSwiggy)
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <button
+                className="btn filter-btn"
+                onClick={()=>{
+                    const filteredList = listOfRestaurants.filter((res)=>
+                        res.card.card.info.avgRating > 4.1
+                    )
+                    //reconcile list of restuarants (using React Fiber)
+                    setListOfRestraunt(filteredList);
+                }}
+            >
+                Top Rated Restaurants
+            </button>
             <div className="res-container">
                 {
-                    cardsFromSwiggy.map((restuarant) => (
+                    listOfRestaurants.map((restuarant) => (
                         <RestuarantCard key={restuarant.card.card.info.id} resData={restuarant.card.card.info} />
 
                     ))
